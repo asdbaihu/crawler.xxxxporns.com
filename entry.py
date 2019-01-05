9973ef65d39d3c9a68ba76ee2ce947d0c0e707b2
JimCaptain <honeyjimcui@gmail.com>
2019-01-05 21:55:03 +0800

    code adjust.

--- entry.py
+++ entry.py
@@ -4,31 +4,27 @@ from utility import model
 
 
 def main():
+
     crawler_url = config_crawler.BASE_CRAWLER_URL + config_crawler.CRAWLER_URI
 
     mysql_connection = model.mysql_connect()
 
-    while True:
+    page_info = get_page_tracker(mysql_connection)
 
-        page_info = get_page_tracker(mysql_connection)
+    if page_info is None:
+        page = 0
+        model.insert_page_tracker(config_crawler.PAGE_TRACKER_TABLE_NAME, {'page_number': page, 'page_type': 1},
+                                  mysql_connection)
+    else:
+        page = page_info['page_number'] + 1
+        model.update_page_tracker(config_crawler.PAGE_TRACKER_TABLE_NAME, {'page_number': page, 'page_type': 1},
+                                  mysql_connection)
 
-        if page_info is None:
-            page = 0
-            model.insert_page_tracker(config_crawler.PAGE_TRACKER_TABLE_NAME, {'page_number': page, 'page_type': 1},
-                                      mysql_connection)
-        else:
-            page = page_info['page_number'] + 1
-            model.update_page_tracker(config_crawler.PAGE_TRACKER_TABLE_NAME, {'page_number': page, 'page_type': 1},
-                                      mysql_connection)
+    datas = handler.get_datas(crawler_url + str(page))
 
-        datas = handler.get_datas(crawler_url + str(page))
-
-        for data in datas:
-            model.data_insert(config_crawler.LIST_DATA_TABLE_NAME, data, mysql_connection)
-            model.insert_tag(config_crawler.TAGS_TABLE_NAME, data['tags'], mysql_connection)
-        else:
-            model.mysql_close(mysql_connection)
-            exit(0)
+    for data in datas:
+        model.data_insert(config_crawler.LIST_DATA_TABLE_NAME, data, mysql_connection)
+        model.insert_tag(config_crawler.TAGS_TABLE_NAME, data['tags'], mysql_connection)
 
     model.mysql_close(mysql_connection)
 
--- job.py
+++ job.py
@@ -11,20 +11,16 @@ def main():
 
     mysql_connection = model.mysql_connect()
 
-    while True:
-        expired_datas = model.get_expired_datas(config_crawler.LIST_DATA_TABLE_NAME, page, page_size, mysql_connection)
+    expired_datas = model.get_expired_datas(config_crawler.LIST_DATA_TABLE_NAME, page, page_size, mysql_connection)
 
-        for expired_data in expired_datas:
-            detail_data = handler.get_detail_info(expired_data['detail_url'])
-            detail_data.update({'expire_time': int(str(time.time()).split('.')[0]) + config_crawler.VALID_TIME_PERIOD})
-            detail_data.update({'file_hash': expired_data['file_hash']})
+    for expired_data in expired_datas:
+        detail_data = handler.get_detail_info(expired_data['detail_url'])
+        detail_data.update({'expire_time': int(str(time.time()).split('.')[0]) + config_crawler.VALID_TIME_PERIOD})
+        detail_data.update({'file_hash': expired_data['file_hash']})
 
-            model.update_detail_info(config_crawler.LIST_DATA_TABLE_NAME, detail_data, mysql_connection)
-        else:
-            model.mysql_close(mysql_connection)
-            exit(0)
+        model.update_detail_info(config_crawler.LIST_DATA_TABLE_NAME, detail_data, mysql_connection)
 
-        page += 1
+    model.mysql_close(mysql_connection)
 
 
 if __name__ == '__main__':

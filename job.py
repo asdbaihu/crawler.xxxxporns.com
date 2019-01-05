4bf75b1262c449c7a7e5558fddf2567290b4e273
JimCaptain <honeyjimcui@gmail.com>
2019-01-06 01:30:58 +0800

    message consumer code adjust.

--- job.py
+++ job.py
@@ -11,16 +11,17 @@ def main():
 
     mysql_connection = model.mysql_connect()
 
-    expired_datas = model.get_expired_datas(config_crawler.LIST_DATA_TABLE_NAME, page, page_size, mysql_connection)
+    while True:
+        expired_datas = model.get_expired_datas(config_crawler.LIST_DATA_TABLE_NAME, page, page_size, mysql_connection)
 
-    for expired_data in expired_datas:
-        detail_data = handler.get_detail_info(expired_data['detail_url'])
-        detail_data.update({'expire_time': int(str(time.time()).split('.')[0]) + config_crawler.VALID_TIME_PERIOD})
-        detail_data.update({'file_hash': expired_data['file_hash']})
+        for expired_data in expired_datas:
+            detail_data = handler.get_detail_info(expired_data['detail_url'])
+            detail_data.update({'expire_time': int(str(time.time()).split('.')[0]) + config_crawler.VALID_TIME_PERIOD})
+            detail_data.update({'file_hash': expired_data['file_hash']})
 
-        model.update_detail_info(config_crawler.LIST_DATA_TABLE_NAME, detail_data, mysql_connection)
+            model.update_detail_info(config_crawler.LIST_DATA_TABLE_NAME, detail_data, mysql_connection)
 
-    model.mysql_close(mysql_connection)
+        page += 1
 
 
 if __name__ == '__main__':

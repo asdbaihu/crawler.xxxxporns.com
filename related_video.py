9943ac2bc8ec58aa4846687a4b06e003c85170e6
JimCaptain <honeyjimcui@gmail.com>
2019-03-17 20:10:11 +0800

    bug fixed.

--- related_video.py
+++ related_video.py
@@ -26,29 +26,30 @@ def get_related_videos():
             related_videos = handler.parse_related_videos(response.text)
 
             for relate_video in related_videos:
-                detail_info = handler.get_detail_info(relate_video['u'])
-                single_dict = {
-                    'detail_url': relate_video['u'],
-                    'list_thumb_url': relate_video['i'],
-                    'title': relate_video['tf'],
-                    'video_duration': relate_video['d'],
-                    'file_hash': hash.hash_with_blake2b(relate_video['u']),
-                    'addtime': int(str(time.time()).split('.')[0]),
-                    'expire_time': int(str(time.time()).split('.')[0]) + config_crawler.VALID_TIME_PERIOD,
+                if detail_info:
+                    detail_info = handler.get_detail_info(relate_video['u'])
+                    single_dict = {
+                        'detail_url': relate_video['u'],
+                        'list_thumb_url': relate_video['i'],
+                        'title': relate_video['tf'],
+                        'video_duration': relate_video['d'],
+                        'file_hash': hash.hash_with_blake2b(relate_video['u']),
+                        'addtime': int(str(time.time()).split('.')[0]),
+                        'expire_time': int(str(time.time()).split('.')[0]) + config_crawler.VALID_TIME_PERIOD,
 
-                    'video_quality_url': ','.join(detail_info['video_quality_url']),
-                    'video_hls_url': detail_info['video_hls_url'],
-                    'detail_thumb_url': ','.join(detail_info['detail_thumb_url']),
-                    'thumb_slide_url': ','.join(detail_info['thumb_slide_url']),
-                    'thumb_slide_minute': detail_info['thumb_slide_minute'],
-                    'cdn_url': detail_info['video_url_cdn'],
-                    'tags': ','.join(detail_info['tags']),
-                    'status': 1,
-                }
+                        'video_quality_url': ','.join(detail_info['video_quality_url']),
+                        'video_hls_url': detail_info['video_hls_url'],
+                        'detail_thumb_url': ','.join(detail_info['detail_thumb_url']),
+                        'thumb_slide_url': ','.join(detail_info['thumb_slide_url']),
+                        'thumb_slide_minute': detail_info['thumb_slide_minute'],
+                        'cdn_url': detail_info['video_url_cdn'],
+                        'tags': ','.join(detail_info['tags']),
+                        'status': 1,
+                    }
 
-                model.data_insert(config_crawler.LIST_DATA_TABLE_NAME, single_dict, mysql_connection)
-                model.set_related_videos(config_crawler.RELATED_VIDEOS_TABLE_NAME, video['id'], single_dict['file_hash'], mysql_connection)
-                
+                    model.data_insert(config_crawler.LIST_DATA_TABLE_NAME, single_dict, mysql_connection)
+                    model.set_related_videos(config_crawler.RELATED_VIDEOS_TABLE_NAME, video['id'], single_dict['file_hash'], mysql_connection)
+
             model.set_have_related_video(config_crawler.LIST_DATA_TABLE_NAME, video['id'], mysql_connection)
 
 

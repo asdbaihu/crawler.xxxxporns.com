db826f18520222e4278c0aa0440bd12c7d6e68cc
JimCaptain <honeyjimcui@gmail.com>
2019-04-07 18:52:44 +0800

    test.

--- related_video.py
+++ related_video.py
@@ -19,7 +19,6 @@ def get_related_videos():
     videos = model.get_videos(config_crawler.LIST_DATA_TABLE_NAME, last_id, config_crawler.PAGE_SIZE_TO_VIDEO, mysql_connection)
 
     if videos:
-        model.set_last_id(config_crawler.PAGE_TRACKER_TABLE_NAME, videos[len(videos) - 1]['id'], mysql_connection)
 
         for video in videos:
             response = requests.get(config_crawler.BASE_CRAWLER_URL + video['detail_url'])
@@ -51,6 +50,9 @@ def get_related_videos():
                     model.set_related_videos(config_crawler.RELATED_VIDEOS_TABLE_NAME, video['id'], single_dict['file_hash'], mysql_connection)
 
             model.set_have_related_video(config_crawler.LIST_DATA_TABLE_NAME, video['id'], mysql_connection)
+
+        model.set_last_id(config_crawler.PAGE_TRACKER_TABLE_NAME, videos[len(videos) - 1]['id'], mysql_connection)
+
 
 
 def get_last_id(mysql_connection):
